e6583633068a02fc2a7a02b1682ea5ba083b786f
Vitaly65 <87527311+Vitaly65@users.noreply.github.com>
2021-07-16 15:44:57 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import discord;
+from discord.ext import commands
+from discord import utils
+import os;
+import config;
+
+bot = commands.Bot(command_prefix='!', intents = discord.Intents.all());
+
+@bot.event
+async def on_ready():
+    print('We have logged as {0.user}');
+
+@bot.event
+async def on_member_join(member):
+    role = utils.get(member.guild.roles, id=config.ROLE);
+    await bot.get_channel(758724426455580685).send(f"У нас новый гражданский!{member.mention}");
+    await member.add_roles(role);
+
+bot.run(config.TOKEN);
