7cdeb69843dbfe3e130f96409a4afe0fd30e2078
Vitaly65 <87527311+Vitaly65@users.noreply.github.com>
2021-09-03 16:03:31 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+TOKEN = '';
+ROLE = 801163825097342976;
+CHANNEL = 758724426455580685;
+botstatus = '❌';
+sqlstatus = '❌';
+
+#SQL settings
+host = "";
+user = "";
+password = "";
+db_name = "";
+port = '';
+charset='utf8';
