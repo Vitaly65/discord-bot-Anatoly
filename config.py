19a79a459acc704c108762f440216eb17218907e
Vitaly <87527311+Vitaly65@users.noreply.github.com>
2021-09-05 00:42:21 +0300

    Update config.py

--- config.py
+++ config.py
@@ -10,9 +10,3 @@ password = 'nDQtaDIatU';
 db_name = 'sql11434537';
 port = 3306;
 charset='utf8';
-
-#Server: sql11.freemysqlhosting.net
-#Name: sql11434537
-#Username: sql11434537
-#Password: nDQtaDIatU
-#Port number: 3306
